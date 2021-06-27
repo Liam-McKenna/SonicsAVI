ef87d6fe3cac2619e7fd064e1cf2fdaedfd8d72e
Liam-McKenna <liam.zendesign@gmail.com>
2021-06-27 01:29:10 +0100

    added email and phone to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,20 @@ import project from "../img/project.svg";
 import customerservice from "../img/customer-service.svg";
 import team from "../img/team.svg";
 import logo from "../img/logoWhite.png";
+import phone from "../img/smartphone.svg";
+import email from "../img/email.svg";
 
 const Navbar = () => {
+  const mailButton = () => {
+    const mailto =
+      "mailto:mail@gmail.com?subject=Test subject&body=Body content";
+    window.location.href = mailto;
+  };
+  const phoneButton = () => {
+    const tel = "tel:083 444 8785";
+    window.location.href = tel;
+  };
+
   return (
     <Nav>
       <div className="logo">
@@ -35,6 +47,14 @@ const Navbar = () => {
           <img src={customerservice} alt="customerservice" />
           <span className="navText">Contact</span>
         </Link>
+        <div className="phoneAndEmail" id="navLink5">
+          <div className="phoneButton" onClick={phoneButton}>
+            <img src={phone} alt="" />
+          </div>
+          <div className="mailButton" onClick={mailButton}>
+            <img src={email} alt="" />
+          </div>
+        </div>
       </div>
     </Nav>
   );
@@ -62,7 +82,7 @@ const Nav = styled.div`
     justify-content: space-between;
     overflow: hidden;
     width: 5rem;
-    height: 20rem;
+    height: 25rem;
     position: fixed;
     background-color: ${secondaryColor};
     border-radius: 0rem 2rem 2rem 0rem;
@@ -71,7 +91,7 @@ const Nav = styled.div`
     }
     &:hover {
       transition: ${transitionTime};
-      width: 16rem;
+      width: 13rem;
       .navLink {
         display: flex;
         flex-direction: row;
@@ -81,6 +101,18 @@ const Nav = styled.div`
         visibility: visible;
         color: white;
         font-weight: bold;
+      }
+      .phoneAndEmail {
+        border-top: 2px solid ${secondaryColor};
+        img {
+          width: 2rem;
+          &:hover {
+            width: 3rem;
+          }
+          &:active {
+            width: 1.9rem;
+          }
+        }
       }
     }
   }
@@ -118,6 +150,57 @@ const Nav = styled.div`
   .navText {
     transition: ${transitionTime};
     visibility: hidden;
+    margin-left: 2rem;
+  }
+
+  .phoneAndEmail {
+    transition: ${transitionTime};
+    text-decoration: none;
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    border-top: 2px solid ${primaryColor};
+    align-items: center;
+
+    &:hover {
+      background-color: ${primaryColor};
+      .navText {
+        color: ${heavyBlue};
+      }
+    }
+    &:active {
+      text-decoration: none;
+    }
+    img {
+      transition: ${transitionTime};
+      margin: 0rem 0.5rem 0rem 0.5rem;
+      width: 90%;
+      height: 100%;
+      width: 1.5rem;
+      &:hover {
+        width: 3rem;
+      }
+      &:active {
+        width: 1.8rem;
+      }
+    }
+    .phoneButton {
+      background-color: #38ffac;
+      height: 100%;
+      width: 100%;
+      display: flex;
+
+      justify-content: center;
+    }
+    .mailButton {
+      background-color: #00a3ff;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
   }
 
   //mobile settings
@@ -130,7 +213,6 @@ const Nav = styled.div`
       /* display: none; */
       visibility: visible;
     }
-
     .links {
       flex-direction: row;
       border-radius: 0;
@@ -170,6 +252,9 @@ const Nav = styled.div`
           /* width: 4rem; */
         }
       }
+    }
+    .phoneAndEmail {
+      display: none;
     }
   }
 `;
